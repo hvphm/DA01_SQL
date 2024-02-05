8689b529289e55016b838c7f21df5d4c23749e7a
hvphm <157187870+hvphm@users.noreply.github.com>
2024-02-05 07:19:26 +0700

    Update DAY 13_HW.sql

--- DAY 13_HW.sql
+++ DAY 13_HW.sql
@@ -29,22 +29,51 @@ FROM pages
 WHERE page_id NOT IN (SELECT page_id FROM page_likes);
 
 -- Ex 5
+WITH june_user AS
+(
+    SELECT  DISTINCT user_id
+    FROM user_actions
+    WHERE EXTRACT(MONTH FROM event_date) = 6
+)
 
+SELECT EXTRACT(MONTH FROM event_date) AS month,
+       COUNT(DISTINCT user_actions.user_id) AS monthly_active_users
+FROM user_actions
+JOIN june_user ON user_actions.user_id = june_user.user_id
+GROUP BY EXTRACT(MONTH FROM event_date)
+HAVING EXTRACT(MONTH FROM event_date) = 7;
 
 -- Ex 6
-
+SELECT DATE_FORMAT(trans_date, '%Y-%m') AS month,
+       country,
+       COUNT(*) AS trans_count,
+       SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+       SUM(amount) AS trans_total_amount,
+       SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM transactions
+GROUP BY month, country;
 
 -- Ex 7
-
+SELECT product_id, year AS first_year, quantity, price
+FROM sales
+WHERE (product_id, year) IN (SELECT product_id, MIN(year) FROM sales GROUP BY product_id);
 
 -- Ex 8
-
+SELECT customer_id
+FROM customer
+WHERE product_key IN (SELECT product_key FROM product)
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM product);
 
 -- Ex 9
-
+SELECT employee_id
+FROM employees
+WHERE salary < 30000
+AND manager_id NOT IN (SELECT employee_id FROM employees)
+ORDER BY employee_id;
 
 -- Ex 10
-
+-- Bài tập bị gắn nhầm link của Exercise 1
 
 -- Ex 11
 
